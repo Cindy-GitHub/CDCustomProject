8a5b52dae604ea79f588f251cc012c94efef829b
Chendi <di.chen@innonetwork.com>
2015-05-28 17:24:15 +0800

    init project

---
+++ CustomFunction/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  CustomFunction
+//
+//  Created by Chendi on 15/5/28.
+//  Copyright (c) 2015年 Cindy. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
